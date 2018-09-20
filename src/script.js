7b0675f277db2eef97ac67cded0aa6f5ee98f68d
Cristian Derick <communication-world@live.com>
2018-09-20 13:37:28 +0100

    Added comments

--- src/script.js
+++ src/script.js
@@ -4,12 +4,16 @@ $ = jQuery;
 
     $(function() {
 
+        // Inicia o Script e dispara 3 funcoes
         var app = {
             init: function() {
                 app.initialScripts();
                 app.retriveData();
                 app.filterChange();
             },
+
+            // o Script inicial ele vai esconder as tabs e criar a Dynamic tab de acordo com o que clica na navegacao
+              // se for criar mais tabs ou remover e so seguir o nome da data-gentabs tem o mesmo nome da classe na aba
             initialScripts: function(){
                 $('.hidecontroler').hide();
                 $('.mainsearch').show();
@@ -23,13 +27,19 @@ $ = jQuery;
                   $('.'+tt).show();
                 });
             },
+
+            //retrive data esta pegando o Json e transformando em objeto em seguida passando esse objeto para a outra funcao loadHydrogen
             retriveData: function() {
                 $.getJSON('src/medicamentos.json').done(function(json) {
                     allData = json;
                     app.loadHydrogen(allData);
                 });
             },
+
+
             loadHydrogen: function(allData) {
+
+              // HELPERS do HANDLEBAR caso necessario.... Nao usei nenhum... se quiser remover (y) go ahead
                   Handlebars.registerHelper('compare', function (lvalue, operator, rvalue, options) {
                     var operators, result;
                     if (arguments.length < 3) {
@@ -116,19 +126,36 @@ $ = jQuery;
                     else
                         return opts.inverse(this);
                 });
+
+                // FIM DOS HELPERS DO HANDLEBARS
+
+                // Nessa funcao ele esta pegando o template med.html compilando com o Json que foi recebido no inicio do script
+                  // E compilando transformando em um HTML com todos os dados
+                  // Em seguida ele renderiza dentro do id tableControler
+
                 $.get('templates/med.html', function(template) {
                     var theTemplate = Handlebars.compile(template);
                     var rendered = theTemplate(allData);
                     $('#tableControler').html(rendered);
                 });
+
+                // Nessa funcao ele esta pegando o template modal.html compilando com o Json que foi recebido no inicio do script
+                  // E compilando transformando em um HTML com todos os dados
+                  // Em seguida ele renderiza dentro do id rendermodais
+
                 $.get('templates/modal.html', function(template) {
                     var theTemplate = Handlebars.compile(template);
                     var rendered = theTemplate(allData);
                     $('#rendermodais').html(rendered);
                 });
             },
+
+            // Funcao principal para pesquisa
+              // esconde todo o conteudo 
             filterChange: function() {
               $('#tableControl').hide();
+
+              // se o enter for pressionado ele ativa o click do botao de pesquisa
               $('#genericSearch').keypress(function (e) {
                   if (e.which == '13') {
                       e.preventDefault();
@@ -136,12 +163,17 @@ $ = jQuery;
                       $('#filter-medicine').click();
                   }
               });
+              // quando o botao de pesquisa for clicado ele pega o valor do input e passa como parametro pra funcao applyFilter
               $('#filter-medicine').click(function() {
                 var selectedMedicine = $('#genericSearch').val();
                 $('#tableControl').show();
                 app.applyFilter(selectedMedicine);
               });
             },
+
+            // Recebe o valor do click
+              // vai em cada data da tabela e tenta encontrar quem tem esse valor
+                // quem nao tem o valor ele esconde... quem tem o valor ele mostra
             applyFilter: function(selectedMedicine) {
               var $medvisa = $('#tableControler').find('.medication');
               $medvisa.each(function(i, val) {
@@ -156,6 +188,8 @@ $ = jQuery;
               });
             },
         };
+
+        // inicia o processo inteiro
         app.init();
     });
 })(jQuery);
